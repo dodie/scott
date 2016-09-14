e4c507639d6a094cebd81573cc475caebc6e4326
David Csakvari <dodiehun@gmail.com>
2016-09-14 08:06:46 +0200

    Fix exception message augmentation in run listener to avoid multiple augmentations.

--- scott/src/main/java/hu/advancedweb/scott/runtime/ScottRunListener.java
+++ scott/src/main/java/hu/advancedweb/scott/runtime/ScottRunListener.java
@@ -1,6 +1,8 @@
 package hu.advancedweb.scott.runtime;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.runner.Description;
 import org.junit.runner.notification.Failure;
@@ -16,9 +18,17 @@ public class ScottRunListener extends RunListener {
 	
 	private Description description;
 	
+	/**
+	 * Sometimes testFailure is called multiple times for a single test,
+	 * so we has to track the already augmented Exception objects.
+	 */
+	private Set<Object> augmentedExceptions = new HashSet<>();
+	
 	@Override
 	public void testStarted(Description description) throws Exception {
 		this.description = description;
+		augmentedExceptions.clear();
+		
 		super.testStarted(description);
 	}
 	
@@ -30,19 +40,22 @@ public class ScottRunListener extends RunListener {
 	}
 	
 	private void setExceptionMessage(Object object, Object fieldValue) {
-	    final String fieldName = "detailMessage";
-		Class<?> clazz = object.getClass();
-	    while (clazz != null) {
-	        try {
-	            Field field = clazz.getDeclaredField(fieldName);
-	            field.setAccessible(true);
-	            field.set(object, fieldValue);
-	            return;
-	        } catch (NoSuchFieldException e) {
-	            clazz = clazz.getSuperclass();
-	        } catch (Exception e) {
-	            throw new IllegalStateException(e);
-	        }
-	    }
+		if (!augmentedExceptions.contains(object)) {
+			augmentedExceptions.add(object);
+			final String fieldName = "detailMessage";
+			Class<?> clazz = object.getClass();
+			while (clazz != null) {
+				try {
+					Field field = clazz.getDeclaredField(fieldName);
+					field.setAccessible(true);
+					field.set(object, fieldValue);
+					return;
+				} catch (NoSuchFieldException e) {
+					clazz = clazz.getSuperclass();
+				} catch (Exception e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		}
 	}
 }
