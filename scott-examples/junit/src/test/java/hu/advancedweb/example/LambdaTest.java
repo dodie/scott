a57ae75360c2e54829b1656c829f625baa767f43
David Csakvari <dodiehun@gmail.com>
2016-09-18 11:14:36 +0200

    Add test with lambda

---
+++ scott-examples/junit/src/test/java/hu/advancedweb/example/LambdaTest.java
@@ -0,0 +1,34 @@
+package hu.advancedweb.example;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+
+public class LambdaTest {
+
+	@Test
+	public void test_with_lambda() throws Exception {
+		Function<String, String> generatePalindrome = input -> {
+			StringBuilder sb = new StringBuilder();
+			sb.append(input);
+			sb.reverse();
+			
+			String reversed = sb.toString();
+			
+			String palindrome = input + reversed;
+			return palindrome;
+		};
+		
+		String word = "cat";
+		
+		String palindromized = generatePalindrome.apply(word);
+		
+		assertEquals("Hello World!", "Hello World!!!");
+		
+		assertThat(palindromized, equalTo(word + word));
+	}
+	
+}
