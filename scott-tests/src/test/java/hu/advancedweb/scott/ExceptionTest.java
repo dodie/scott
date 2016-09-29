ffc09320520f729d77cc0bea441e632b884b0624
David Csakvari <dodiehun@gmail.com>
2016-09-29 08:05:11 +0200

    Add tests

--- scott-tests/src/test/java/hu/advancedweb/scott/ExceptionTest.java
+++ scott-tests/src/test/java/hu/advancedweb/scott/ExceptionTest.java
@@ -39,4 +39,25 @@ public class ExceptionTest {
 		}	
 	}
 	
+	// FIXME: if I uncomment this test, it kills the VM
+//	@SuppressWarnings("null")
+//	@Test
+//	public void moreVariablesInTryBlockThanInCatchBlock() {
+//		String o = null;
+//		try {
+//			String inner = "inner";
+//			assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo(inner));
+//			String inner2 = "inner2";
+//			assertThat(TestHelper.getLastRecordedStateFor("inner2"), equalTo(inner2));
+//			o.length();
+//		} catch (Exception e) {
+//			o = "fallback";
+//			// the scope determination is wrong, so without this variable the following assertThat still going to be instrumented
+//			// to query var=3, which is not existent
+////			String x = "";
+//			assertThat(TestHelper.getLastRecordedStateFor("e"), equalTo(e.toString()));
+//			assertThat(TestHelper.getLastRecordedStateFor("o"), equalTo(o));
+//		}
+//	}
+	
 }
--- scott-tests/src/test/java/hu/advancedweb/scott/RecordingTest.java
+++ scott-tests/src/test/java/hu/advancedweb/scott/RecordingTest.java
@@ -67,19 +67,6 @@ public class RecordingTest {
 		assertThat(TestHelper.getLastRecordedStateFor("myClass"), equalTo(myClass.toString()));
 	}
 	
-//	//FIXME: This test kills the whole Test file, nothing is recorded. See Issue #2.
-//	// If I add a single Sysout to the inner block it seems to fix it.
-//	@Test
-//	public void mutationWithBlockThatHasASingleDeclaration() throws Exception {
-//		String outer = "outer";
-//		{
-//			String inner = "inner";
-//		}
-//		
-//		assertThat(TestHelper.getLastRecorderStateFor("inner"), equalTo("inner"));
-//		assertThat(TestHelper.getLastRecorderStateFor("outer"), equalTo("outer"));
-//	}
-	
 	public static class CustomClass {
 		int i;
 		
@@ -92,5 +79,102 @@ public class RecordingTest {
 			return "MyClass [i=" + i + "]";
 		}
 	}
+	
+	@Test
+	public void recordConsecutiveDeclarations() {
+		String inner = "inner";
+		String inner2 = "inner2";
+		assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo(inner));
+		assertThat(TestHelper.getLastRecordedStateFor("inner2"), equalTo(inner2));
+	}
+	
+	@Test
+	public void recordManyDeclarations() {
+		String inner = "inner";
+		String inner2 = "inner2";
+		String inner3 = "inner3";
+		String inner4 = "inner4";
+		String inner5 = "inner5";
+		String inner6 = "inner6";
+		String inner7 = "inner7";
+		String inner8 = "inner8";
+		String inner9 = "inner9";
+		String inner10 = "inner10";
+		assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo(inner));
+		assertThat(TestHelper.getLastRecordedStateFor("inner2"), equalTo(inner2));
+		assertThat(TestHelper.getLastRecordedStateFor("inner3"), equalTo(inner3));
+		assertThat(TestHelper.getLastRecordedStateFor("inner4"), equalTo(inner4));
+		assertThat(TestHelper.getLastRecordedStateFor("inner5"), equalTo(inner5));
+		assertThat(TestHelper.getLastRecordedStateFor("inner6"), equalTo(inner6));
+		assertThat(TestHelper.getLastRecordedStateFor("inner7"), equalTo(inner7));
+		assertThat(TestHelper.getLastRecordedStateFor("inner8"), equalTo(inner8));
+		assertThat(TestHelper.getLastRecordedStateFor("inner9"), equalTo(inner9));
+		assertThat(TestHelper.getLastRecordedStateFor("inner10"), equalTo(inner10));
+	}
 
+	@Test
+	public void methodWithJustADeclarationDontCrash() {
+		String outer = "outer!";
+	}
+	
+	@Test
+	public void recordMethodWithJustADeclaration() {
+		String outer = "outer!";
+		assertThat(TestHelper.getLastRecordedStateFor("outer"), equalTo(outer));
+	}
+	
+	@Test
+	public void recordMethodWithJustADeclarationInABlock() {
+		{
+			String inner = "inner";
+			assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo(inner));
+		}
+	}
+
+//	//FIXME: This test kills the whole Test file, nothing is recorded.
+//	@Test
+//	public void methodWithJustADeclarationInABlockDontCrash() {
+//		{
+//			String inner = "inner";
+//		}
+//	}
+	
+//	//FIXME: This test kills the whole Test file, nothing is recorded. See Issue #2.
+//	// If I add a single Sysout to the inner block it seems to fix it.
+//	@Test
+//	public void methodWithJustADeclarationInABlockDontCrash2() throws Exception {
+//		String outer = "outer";
+//		{
+//			String inner = "inner";
+//		}
+//		
+//		assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo("inner"));
+//		assertThat(TestHelper.getLastRecordedStateFor("outer"), equalTo("outer"));
+//	}
+	
+//	@Test
+//	public void inlineFormat2tingWithTryCatch() {
+//		try {
+//			String inner = "inner";
+//			assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo(inner));
+//			String inner2 = "inner2";
+//			assertThat(TestHelper.getLastRecordedStateFor("inner2"), equalTo(inner2));
+//		} catch (Exception e) {
+//			assertThat(TestHelper.getLastRecordedStateFor("e"), equalTo(e.toString()));
+//		}
+//	}
+	
+//	@Test
+//	@Ignore
+//	public void inlineFormat2tingWithTryCatch2() {
+//		try {
+//			String inner = "inner";
+////			assertThat(TestHelper.getLastRecordedStateFor("inner"), equalTo(inner));
+//			String inner2 = "inner2";
+////			assertThat(TestHelper.getLastRecordedStateFor("inner2"), equalTo(inner2));
+//		} catch (Exception e) {
+////			assertThat(TestHelper.getLastRecordedStateFor("e"), equalTo(e.toString()));
+//		}
+//	}
+	
 }
