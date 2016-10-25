dff6ab28d08dcadaa8485e16c54b0df263810dc5
David Csakvari <david.csakvari@ponte.hu>
2016-10-25 17:54:45 +0200

    Fix test

--- scott-tests/src/test/java/hu/advancedweb/scott/MethodNameAndClassTrackingTest.java
+++ scott-tests/src/test/java/hu/advancedweb/scott/MethodNameAndClassTrackingTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
-import org.objectweb.asm.Type;
 
 import hu.advancedweb.scott.runtime.track.StateRegistry;
 
@@ -12,13 +11,13 @@ public class MethodNameAndClassTrackingTest {
 	
 	@Test
 	public void test1() throws Exception {
-		assertThat(StateRegistry.getTestClassType(), equalTo(Type.getInternalName(MethodNameAndClassTrackingTest.class)));
+		assertThat(StateRegistry.getTestClassType(), equalTo("hu/advancedweb/scott/MethodNameAndClassTrackingTest"));
 		assertThat(StateRegistry.getTestMethodName(), equalTo("test1"));
 	}
 
 	@Test
 	public void test2() throws Exception {
-		assertThat(StateRegistry.getTestClassType(), equalTo(Type.getInternalName(MethodNameAndClassTrackingTest.class)));
+		assertThat(StateRegistry.getTestClassType(), equalTo("hu/advancedweb/scott/MethodNameAndClassTrackingTest"));
 		assertThat(StateRegistry.getTestMethodName(), equalTo("test2"));
 	}
 
