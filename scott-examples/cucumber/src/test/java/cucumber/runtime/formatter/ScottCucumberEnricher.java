1855f920e39ee1608239a0cc2c7462e99e04c118
basalt79 <Rapid_git_1318>
2017-10-08 19:15:27 +0200

    Record whole scenarios with Cucumber #12

--- scott-examples/cucumber/src/test/java/cucumber/runtime/formatter/ScottCucumberEnricher.java
+++ scott-examples/cucumber/src/test/java/cucumber/runtime/formatter/ScottCucumberEnricher.java
@@ -3,6 +3,8 @@ package cucumber.runtime.formatter;
 import gherkin.formatter.model.Result;
 import hu.advancedweb.scott.runtime.report.FailureRenderer;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,17 +18,26 @@ public class ScottCucumberEnricher {
 
     public Result result(Result result) {
         String errorMessage = result.getErrorMessage();
-        String dummyFailure = FailureRenderer.render(null, null);
         if (result.getStatus().equals(Result.FAILED)) {
             StringBuilder builder = new StringBuilder();
             for (String dummy : informationList) {
                 builder.append(dummy);
             }
-            builder.append(dummyFailure).append(errorMessage);
+            builder.append(getErrorMessage(result.getError()));
             errorMessage = builder.toString();
         } else {
+            String dummyFailure = FailureRenderer.render(null, null);
             informationList.add(dummyFailure);
         }
         return new Result(result.getStatus(), result.getDuration(), errorMessage);
+    }
+
+    private String getErrorMessage(Throwable error) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        error.printStackTrace(printWriter);
+        String msg = stringWriter.getBuffer().toString();
+        msg = msg.substring(msg.indexOf("\n"));
+        return msg;
     }
 }
