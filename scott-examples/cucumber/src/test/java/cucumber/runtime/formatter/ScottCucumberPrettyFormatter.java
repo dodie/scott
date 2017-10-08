ca26630ad3d5e4ef0c27cb890c39f71f2f3cd53e
basalt79 <Rapid_git_1318>
2017-10-08 09:38:22 +0200

    Record whole scenarios with Cucumber #12

--- scott-examples/cucumber/src/test/java/cucumber/runtime/formatter/ScottCucumberPrettyFormatter.java
+++ scott-examples/cucumber/src/test/java/cucumber/runtime/formatter/ScottCucumberPrettyFormatter.java
@@ -1,16 +1,26 @@
 package cucumber.runtime.formatter;
 
 import gherkin.formatter.model.Result;
+import gherkin.formatter.model.Scenario;
 
 public class ScottCucumberPrettyFormatter extends CucumberPrettyFormatter {
 
+	private final ScottCucumberEnricher enricher;
+
 	public ScottCucumberPrettyFormatter(Appendable out) {
 		super(out);
+		enricher = new ScottCucumberEnricher();
+	}
+
+	@Override
+	public void scenario(Scenario scenario) {
+		enricher.scenario();
+		super.scenario(scenario);
 	}
 
 	@Override
 	public void result(Result result) {
-		super.result(new Result(result.getStatus(), result.getDuration(), result.getError(), null));
+		super.result(enricher.result(result));
 	}
 
 }
--- scott-examples/cucumber/src/test/java/cucumber/runtime/formatter/ScottHTMLFormatter.java
+++ scott-examples/cucumber/src/test/java/cucumber/runtime/formatter/ScottHTMLFormatter.java
@@ -1,18 +1,28 @@
 package cucumber.runtime.formatter;
 
-import java.net.URL;
-
 import gherkin.formatter.model.Result;
+import gherkin.formatter.model.Scenario;
+
+import java.net.URL;
 
 public class ScottHTMLFormatter extends HTMLFormatter {
 
+	private final ScottCucumberEnricher enricher;
+
 	public ScottHTMLFormatter(URL htmlReportDir) {
 		super(htmlReportDir);
+		enricher = new ScottCucumberEnricher();
+	}
+
+	@Override
+	public void scenario(Scenario scenario) {
+		enricher.scenario();
+		super.scenario(scenario);
 	}
 
 	@Override
 	public void result(Result result) {
-		super.result(new Result(result.getStatus(), result.getDuration(), result.getError(), null));
+		super.result(enricher.result(result));
 	}
 
 }
