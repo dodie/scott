e7a5f4c25477b1b1cb6fd971e1fab76f416a7156
Marvin Herman Froeder <marvin@thelastpickle.com>
2017-10-23 09:14:44 +1300

    Create arguments to scott

--- scott-maven-plugin/src/hu/advancedweb/maven/ScottAgentMojo.java
+++ scott-maven-plugin/src/hu/advancedweb/maven/ScottAgentMojo.java
@@ -11,10 +11,12 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import static java.lang.String.format;
+import static org.codehaus.plexus.util.StringUtils.join;
 
 @Mojo(name = "prepare-agent", defaultPhase = LifecyclePhase.INITIALIZE,
         requiresDependencyResolution = ResolutionScope.RUNTIME,
@@ -30,6 +32,15 @@ public class ScottAgentMojo extends AbstractMojo {
     @Parameter(property = "plugin.artifactMap", required = true, readonly = true)
     Map<String, Artifact> pluginArtifactMap;
 
+    @Parameter(property = "scott.track.method_annotation")
+    List<String> trackMethodAnnotations;
+
+    @Parameter(property = "scott.inject_junit4_rule.method_annotation")
+    List<String> junit4RuleMethodAnnotations;
+
+    @Parameter(property = "scott.inject_junit5_extension.method_annotation")
+    List<String> junit5RuleMethodAnnotations;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
         final Properties projectProperties = this.project.getProperties();
         final File agentJarFile = pluginArtifactMap.get(SCOTT_ARTIFACT_NAME).getFile();
@@ -39,7 +50,17 @@ public class ScottAgentMojo extends AbstractMojo {
     }
 
     private String getArgument(final File agentJarFile) {
-        return format("-javaagent:%s", agentJarFile);
+        return format("-javaagent:%s", agentJarFile)
+            + createScottArgument("scott.track.method_annotation", trackMethodAnnotations)
+            + createScottArgument("scott.inject_junit4_rule.method_annotation", junit4RuleMethodAnnotations)
+            + createScottArgument("scott.inject_junit5_extension.method_annotation", junit5RuleMethodAnnotations);
+    }
+
+    private String createScottArgument(String name, List<String> arguments) {
+        if (arguments == null || arguments.isEmpty())
+            return "";
+
+        return format(" -D%s=\"%s\"", name, join(arguments.iterator(), ","));
     }
 
 }
