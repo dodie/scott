9570c66a4b9ab4215e34667bb29c476f1a268e7e
Kevin Kraus <kraus.kevin@gmail.com>
2018-11-13 20:54:12 -0600

    additional test for a single resource

--- scott-tests/junit4-tests/src/test/java/hu/advancedweb/scott/TryWithResourcesRecordingTest.java
+++ scott-tests/junit4-tests/src/test/java/hu/advancedweb/scott/TryWithResourcesRecordingTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.StringWriter;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -12,7 +13,17 @@ import static org.junit.Assert.assertThat;
 public class TryWithResourcesRecordingTest {
 
     @Test
-    public void try_with_resources() throws IOException {
+    public void try_with_resources_one_resource() throws IOException {
+        try (StringWriter out = new StringWriter()) {
+
+            out.write("Hello World");
+
+            assertThat(TestHelper.getLastRecordedStateForVariable("out"), equalTo(out.toString()));
+        }
+    }
+
+    @Test
+    public void try_with_resources_two_resources() throws IOException {
         try (ByteArrayOutputStream out = new ByteArrayOutputStream();
              PrintStream printStream = new PrintStream(out)) {
 
