94fad824bf007fb6716d1c7fe12f58c6a3a439f6
David Csakvari <dodiehun@gmail.com>
2019-02-20 19:54:23 +0100

    Fix formatting

--- scott/src/main/java/hu/advancedweb/scott/runtime/ScottCucumberEnricher.java
+++ scott/src/main/java/hu/advancedweb/scott/runtime/ScottCucumberEnricher.java
@@ -12,34 +12,35 @@ class ScottCucumberEnricher {
 
 	private List<String> informationList;
 
-    public void scenario() {
-        informationList = new ArrayList<>();
-    }
+	public void scenario() {
+		informationList = new ArrayList<>();
+	}
 
-    public Result result(Result result) {
-        String errorMessage = result.getErrorMessage();
-        if (result.getStatus().equals(Result.FAILED)) {
-            String errorString = FailureRenderer.render(null, null, result.getError());
-            StringBuilder builder = new StringBuilder();
-            for (String info : informationList) {
-                builder.append(info);
-            }
-            builder.append(errorString);
-            builder.append(getErrorMessage(result.getError()));
-            errorMessage = builder.toString();
-        } else {
-            String errorString = FailureRenderer.render(null, null);
-            informationList.add(errorString);
-        }
-        return new Result(result.getStatus(), result.getDuration(), errorMessage);
-    }
+	public Result result(Result result) {
+		String errorMessage = result.getErrorMessage();
+		if (result.getStatus().equals(Result.FAILED)) {
+			String errorString = FailureRenderer.render(null, null, result.getError());
+			StringBuilder builder = new StringBuilder();
+			for (String info : informationList) {
+				builder.append(info);
+			}
+			builder.append(errorString);
+			builder.append(getErrorMessage(result.getError()));
+			errorMessage = builder.toString();
+		} else {
+			String errorString = FailureRenderer.render(null, null);
+			informationList.add(errorString);
+		}
+		return new Result(result.getStatus(), result.getDuration(), errorMessage);
+	}
 
-    private String getErrorMessage(Throwable error) {
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);
-        error.printStackTrace(printWriter);
-        String msg = stringWriter.getBuffer().toString();
-        msg = msg.substring(msg.indexOf("\n"));
-        return msg;
-    }
+	private String getErrorMessage(Throwable error) {
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stringWriter);
+		error.printStackTrace(printWriter);
+		String msg = stringWriter.getBuffer().toString();
+		msg = msg.substring(msg.indexOf("\n"));
+		return msg;
+	}
+
 }
