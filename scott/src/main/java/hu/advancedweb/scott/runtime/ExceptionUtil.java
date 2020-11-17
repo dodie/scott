99e48e90b2c013e95e1ef9db2516143bf766d37e
Dávid Csákvári <dodiehun@gmail.com>
2020-11-17 10:55:44 +0100

    Add example violations to test CodeQL

--- scott/src/main/java/hu/advancedweb/scott/runtime/ExceptionUtil.java
+++ scott/src/main/java/hu/advancedweb/scott/runtime/ExceptionUtil.java
@@ -4,11 +4,15 @@ import java.lang.reflect.Field;
 
 class ExceptionUtil {
 	
+	private String unused = "unused";
+	private String password = "1234";
+	
 	private ExceptionUtil() {
 		// Utility class, use static methods.
 	}
 	
 	static void setExceptionMessage(Object object, Object fieldValue) {
+		System.out.println(password);
 		final String fieldName = "detailMessage";
 		Class<?> clazz = object.getClass();
 		while (clazz != null) {
